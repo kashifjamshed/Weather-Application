b7f1a7893737288a0abc36ebb495938e417a71e0
Kashif Jamshed <talktokashiif@gmail.com>
2020-07-22 02:56:36 +0530

    commit 2

--- src/app.js
+++ src/app.js
@@ -7,23 +7,23 @@ const forecast = require('./utils/forecast')
 const app = express()
 const port = process.env.PORT || 3000
 
-// Define paths for Express config
+
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
-// Setup handlebars engine and views location
+
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
-// Setup static directory to serve
+
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Kashif Jamshed'
+             name: 'Kashif Jamshed'
     })
 })
 
@@ -31,15 +31,15 @@ app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
         name: 'Kashif Jamshed'
-    })
+})
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
-        title: 'Help',
+      title: 'Help',
         name: 'Kashif Jamshed'
-    })
+ })
 })
 
 app.get('/weather', (req, res) => {
